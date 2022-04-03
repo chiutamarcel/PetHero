e1d58e25b8284f59cb4077d448d49649c0328eb0
ANDREI\Andrei <andreidodot@yahoo.com>
2022-04-03 07:21:19 +0300

    ChatSystem

--- PetHero/Program.cs
+++ PetHero/Program.cs
@@ -53,7 +53,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Chat}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 
